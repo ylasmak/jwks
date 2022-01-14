0b505d40c82e11ebca0a86d4b2eea7a45e9ba0e3
Youness Lasmak <youness@Younesss-MacBook-Pro.local>
2022-01-14 16:34:59 -0500

    jwt

--- 00-install-istio.sh
+++ 00-install-istio.sh
@@ -6,6 +6,14 @@ TOKEN=$(curl https://raw.githubusercontent.com/istio/istio/release-1.12/security
 
 kubectl exec "$(kubectl get pod -l app=sleep -n istio-poc -o jsonpath={.items..metadata.name})" -c sleep -n istio-poc -- curl "http://httpbin.istio-poc:8000/headers" -sS -o /dev/null -H "Authorization: Bearer $TOKEN" -w "%{http_code}\n"
 
+TOKEN2=$(cat token.jwt) && echo "$TOKEN2" | cut -d '.' -f2 - | base64 --decode -
+
+kubectl exec "$(kubectl get pod -l app=sleep -n istio-poc -o jsonpath={.items..metadata.name})" -c sleep -n istio-poc -- curl "http://httpbin.istio-poc:8000/headers" -sS -o /dev/null -H "Authorization: Bearer $TOKEN2" -w "%{http_code}\n"
+
+
 #jwt.gateway.k8s.local
+https://github.com/istio/istio/release-1.12/security/tools/jwt/samples/jwks.json
+
+https://raw.githubusercontent.com/ylasmak/jwks/main/jwks.mini.json
 
 
